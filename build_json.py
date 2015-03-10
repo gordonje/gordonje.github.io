b56d0645dcd8c758563adb3c02cd5b1c0f745306
James Gordon <gordo@mwc-146219.dhcp.missouri.edu>
2015-03-10 15:57:39 -0500

    v1 for json

--- build_json.py
+++ build_json.py
@@ -3,22 +3,9 @@ from getpass import getpass
 import psycopg2
 import psycopg2.extras
 from datetime import datetime
+import json
 
-class Legislator:
-	def __init__(self, **kwargs):
-		self.id = get('id')
-		self.name_first = get('name_first')
-		self.name_middle = get('name_middle')
-		self.name_last = get('name_last')
-		self.name_suffix = get('name_suffix')
-		self.nickname = get('nickname')
-		self.photo_url = get('photo_url')
-		self.current_chamber = get('current_chamber')
-		self.current_title = get('current_title')
-		self.current_abbr_title = get('current_abbr_title')
-		self.current_district = get('current_district')
-		self.current_party = get('current_party')
-
+# connect to the database, prompt if not provided
 try:
 	db = argv[1]
 except IndexError:
@@ -36,46 +23,93 @@ except IndexError:
 
 conn_string = "dbname=%(db)s user=%(user)s password=%(password)s" % {"db": db, "user": user, "password": password}
 
+
 output =  {
-	  'last_updated': datetime.now()
+	  'last_updated': str(datetime.now())
+	, 'parties': []
 	, 'stages': []
 	, 'legislators': []
 }
 
+# add an array of parties to top level of the output
+with psycopg2.connect(conn_string) as conn:
+	with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+		cur.execute('''SELECT id, LOWER(name) as name
+						FROM legislative.legislative_parties;''')
+
+		for party in cur.fetchall():
+			output['parties'].append(party)
+
+# add an array of stages to top level of the output
+with psycopg2.connect(conn_string) as conn:
+	with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+		cur.execute('''SELECT 
+							  id
+							, display_name as name
+							, SUBSTRING(LOWER(display_name) from 0 for 4) as abbrv
+							, position
+						FROM bills.os_action_types
+						WHERE display_name IS NOT NULL
+						ORDER BY position;''')
+
+		for stage in cur.fetchall():
+			# for the total number of bills at this stage
+			stage['num_bills'] = 0
+
+			# for each party, add a key to hold the number of bills sponsored by that party at that stage
+			for party in output['parties']:
+
+				stage['num_' + party['name'] + '_spon'] = 0
+
+			output['stages'].append(stage)
+
+# add an array of legislators
 with psycopg2.connect(conn_string) as conn:
 	with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
 		cur.execute(open('sql/select_legislators.sql', 'r').read())
 
-		for i in cur.fetchall():
+		# for each legislator, add an array of sessions
+		for leg in cur.fetchall():
 
-			leg = Legislator(i)
+			leg["sessions"] = []
 
-			print leg.id
+			cur.execute(open('sql/select_sessions.sql', 'r').read(), (leg['id'], ))
 
-			cur.execute('''SELECT
-							  districts.name::INT as district
-							, legislative_parties.name as party
-							, os_name as os_chamber
-							, chambers.title as title
-							, chambers.abbr_title as abbr_title
-							, sessions.id as session_id
-							, sessions.display_name as session
-							, sessions.name::INT as session_year 
-						FROM legislative.legislator_appointments
-						JOIN legislative.districts
-						ON districts.id = district_id
-						JOIN legislative.legislative_terms
-						ON legislative_terms.id = term_id
-						JOIN legislative.legislative_parties
-						ON legislative_parties.id = party_id
-						JOIN legislative.chambers
-						ON chambers.id = chamber_id
-						JOIN legislative.sessions
-						ON legislative_terms.id = sessions.term_id
-						WHERE legislator_id = %s;''', 
-						(legislator['id']))
+			# append the sessions of each legislator
+			for ses in cur.fetchall():
 
-			legislator['sessions'].a
+				# for each session, get the number of bills sponsored by that legislator that made it to that stage
+				for stage in output['stages']:
+
+					cur.execute(open('sql/count_spon_bills.sql', 'r').read(), {
+						  'legislator_id': leg['id']
+						, 'session_id': ses['session_id']
+						, 'stage_id': stage['id']
+						, 'sponsor_type_id': 15 # "primary" sponsor_type
+					})
+
+					ses['num_' + stage['name'][:3] + '_spon'] = cur.fetchone()['count']
+
+					cur.execute(open('sql/count_spon_bills.sql', 'r').read(), {
+						  'legislator_id': leg['id']
+						, 'session_id': ses['session_id']
+						, 'stage_id': stage['id']
+						, 'sponsor_type_id': 16 # "co-sponsor" sponsor_type
+					})
+
+					ses['num_' + stage['name'][:3] + '_cospon'] = cur.fetchone()['count']
+
+				# for each session, get the number of bills co-sponsored by that legislator that made it to that stage
+
+				leg['sessions'].append(ses)
+
+			output['legislators'].append(leg)
+
+
+# write to json_file
+jsonFile = open('js/data.json', 'w')
+jsonFile.write(json.dumps(output))
+jsonFile.close()
 
 
 
